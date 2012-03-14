97b8bb364708a0654e75d7eb98558e5458b737fb
Casey Li <casey.li@gmail.com>
2012-03-13 22:40:36 -0400

    Generated skelecton Shopify_App using shopify_app gem. Edited for Rails 3.1 deployment to heroku.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sidexside::Application.config.secret_token = 'aee9d13fadf83f7cb0e90c7007cd47b545aa50489303ea59f92c748093ce94cc44ed89a043f0f923981b645c3042202c6b910441d2d8bb5aea11072a1f8bc2d2'

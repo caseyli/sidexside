fffadfa505922105ae67cd45337c4c1694fea603
Casey Li <casey.li@gmail.com>
2012-04-20 17:10:02 -0400

    Fixed case for double-click to edit value for merging.

--- app/assets/javascripts/utilities.js
+++ app/assets/javascripts/utilities.js
@@ -14,6 +14,9 @@ function merge(type, number, key_name) {
 	/* Replace Edit Box with value from opposite item */
 	var opposite_number = parseInt(number)%2 + 1;
 	var merge_value = $("#" + type + "_" + opposite_number.toString() + "_" + key_name).children(".editable").html();
+	if(merge_value == "&lt;double-click to edit&gt;") {
+		merge_value = "";
+	}
 	$("#input-" + number + "-" + key_name).val(merge_value);
 	
 	/* Disable Merge Button */

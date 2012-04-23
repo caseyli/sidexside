d3113d65f3a3c72570be52b560b3e76117a7f0bc
Casey Li <casey.li@gmail.com>
2012-04-22 23:29:17 -0400

    Added Help, Feedback, and About pages.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,8 +11,6 @@ class HomeController < ApplicationController
     @show_logo = true
     @header_sub_message = 'A simple app to let you <strong>compare</strong> information
             about your products.'
-    
-    
   end
   
   def compare_products
@@ -23,6 +21,28 @@ class HomeController < ApplicationController
   def compare_variants
     @header_message = '<strong>Compare Variants</strong>'
     @variant_keys = VariantKey.all
+  end
+  
+  def help
+    @header_message = '<strong>Help and Support</strong>'
+  end
+  
+  def feedback
+    @header_message = '<strong>Feedback</strong>'
+    
+     if !params[:email_address].blank? && !params[:message].blank?
+      FeedbackMailer.feedback_email("Sidexside Feedback from " + params[:email_address],
+                                  params[:email_address],
+                                  params[:message]).deliver
+      @success = true
+    else
+      @sucess = false      
+    end  
+    
+  end
+  
+  def about
+    @header_message = '<strong>About</strong>'
   end
   
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,14 @@ Sidexside::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  # GMail Configuration
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['SMTP_USER'],
+    :password             => ENV['SMTP_PASSWORD'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
